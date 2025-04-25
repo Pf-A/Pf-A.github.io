aa66eab56d102d07d03b4f799cba43957111fd2e
Pf-A <M__carlson1345@outlook.com>
2025-04-25 14:53:13 -0500

    Create lists.js

---
+++ music/js/lists.js
@@ -0,0 +1,17 @@
+function showBenefits() {
+            document.getElementById("benefits").style.display = "block";
+            document.getElementById("drawbacks").style.display = "none";
+            document.getElementById("FAQs").style.display = "none";
+        }
+
+        function showDrawbacks() {
+            document.getElementById("benefits").style.display = "none";
+            document.getElementById("drawbacks").style.display = "block";
+            document.getElementById("FAQs").style.display = "none";
+        }
+
+        function showFAQs() {
+            document.getElementById("benefits").style.display = "none";
+            document.getElementById("drawbacks").style.display = "none";
+            document.getElementById("FAQs").style.display = "block";
+        }
