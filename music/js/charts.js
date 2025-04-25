040795a440615b7b5b8cc4e45e2879e0648d4595
Pf-A <M__carlson1345@outlook.com>
2025-04-25 14:52:16 -0500

    Create charts.js

---
+++ music/js/charts.js
@@ -0,0 +1,44 @@
+function loaded() {
+  console.log('Loaded charts.js');
+}
+        function showChart1() {
+            document.getElementById("chart1").style.display = "block";
+            document.getElementById("chart2").style.display = "none";
+            document.getElementById("chart3").style.display = "none";
+            document.getElementById("chart4").style.display = "none";
+            document.getElementById("chart5").style.display = "none";
+
+
+        }
+        function showChart2() {
+            document.getElementById("chart1").style.display = "none";
+            document.getElementById("chart2").style.display = "block";
+            document.getElementById("chart3").style.display = "none";
+            document.getElementById("chart4").style.display = "none";
+            document.getElementById("chart5").style.display = "none";
+
+        }
+        function showChart3() {
+            document.getElementById("chart1").style.display = "none";
+            document.getElementById("chart2").style.display = "none";
+            document.getElementById("chart3").style.display = "block";
+            document.getElementById("chart4").style.display = "none";
+            document.getElementById("chart5").style.display = "none";
+
+        }
+        function showChart4() {
+            document.getElementById("chart1").style.display = "none";
+            document.getElementById("chart2").style.display = "none";
+            document.getElementById("chart3").style.display = "none";
+            document.getElementById("chart4").style.display = "block";
+            document.getElementById("chart5").style.display = "none";
+        }
+        function showChart5() {
+            document.getElementById("chart1").style.display = "none";
+            document.getElementById("chart2").style.display = "none";
+            document.getElementById("chart3").style.display = "none";
+            document.getElementById("chart4").style.display = "none";
+            document.getElementById("chart5").style.display = "block";
+
+        }
+        
