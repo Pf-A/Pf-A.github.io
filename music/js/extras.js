889b1e9e3d091fe9961a99151e6b8c9b3161d404
Pf-A <M__carlson1345@outlook.com>
2025-05-07 09:24:33 -0500

    Changed mind and made developer things external

--- music/js/extras.js
+++ music/js/extras.js
@@ -21,4 +21,74 @@ function checkConsoleWarning() {
         function beginBootloader() {
             console.log('Found Bootloader.dat! ')
         }
+function cleanUp() {
+            console.clear();
+            cld = "Cleaning up in ";
+        let countdown = 5;
+        const interval = setInterval(() => {
+            console.log(cld + countdown);
+            countdown--;
+            if (countdown < 0) {
+                clearInterval(interval);
+                console.clear();
+                console.log('Cleanup complete.');
+            }
+        }, 1000);
+        }
+    </script>
+    <script>
+            function enableDev() {
+                console.log('Enter the password: (hint: contains "i")');
+                const password = prompt('Enter password:');
+                if (password === 'i7DAxSt4') {
+                    console.clear();
+                    console.log('Developer Mode Activated');
+                    console.log('Commands:');
+                    console.log('autoexec(), showBenefits(), showDrawbacks(), showFAQs(), showChart1(), showChart2(), showChart3(), showChart4(), showChart5(), devInfo()');
+                    console.log('To exit developer mode, refresh the page.');
+                } else {
+                    console.log('Incorrect password. Developer mode not activated.');
+                }
+            }
+            function secret1() {
+                console.log('Enter the password: (hint: "MSI Afterburner")');
+                const password = prompt('Enter the password: (hint: "MSI Afterburner")');
+                if (password === 'overclocking') {
+                    console.log('CORRECT PASSWORD')
+                } else {
+                    console.log('Incorrect password. Developer mode not activated.');
+                }
+            }
+            function forceShow() {
+                const elementName = prompt('Enter the ID of the element you want to show: (Case sensitive)');
+                const element = document.getElementById(elementName);
+                if (element) {
+                    element.style.display = 'block';
+                    alert('Element with ID ${elementName} is now visible.');
+                } else {
+                    alert('No element found with ID "${elementName}".');
+                }
+            }
+            function listFunctions(Serious) {
+                console.clear();
+                if (Serious ===  true or Serious === 1 ) { 
+                } else {
+                console.log('Available functions:');
+                console.log('autoexec() - Runs the OnLoad script.');
+                console.log('cleanUp() - Cleans up the console with a countdown.');
+                console.log('enableDev() - Activates developer mode.');
+                console.log('secret1() - Prompts for a secret password.');
+                console.log('forceShow() - Forces an element to be displayed by ID. Case sensitive.');
+                console.log('showBenefits() - Displays the benefits section.');
+                console.log('showDrawbacks() - Displays the drawbacks section.');
+                console.log('showFAQs() - Displays the FAQs section.');
+                console.log('showChart1() - Displays the first chart.');
+                console.log('showChart2() - Displays the second chart.');
+                console.log('showChart3() - Displays the third chart.');
+                console.log('showChart4() - Displays the fourth chart.');
+                console.log('showChart5() - Displays the fifth chart.');
+                console.log('devInfo() - Displays developer information (if implemented).');
+                }
+            }
+// decided 
 // developer functions. grayed out because it should be inline, for non internet use. 
