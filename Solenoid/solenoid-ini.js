bde307dbb8b093c4d9be9a507e4d4013f3fb197d
Pf-A <M__carlson1345@outlook.com>
2025-05-07 09:53:20 -0500

    Update solenoid-ini.js

--- Solenoid/solenoid-ini.js
+++ Solenoid/solenoid-ini.js
@@ -1,5 +1,5 @@
 function loadedSolenoidINI() {
   console.log('Loaded Solenoid-INI.js');
-              console.log('Solenoid has not been loaded. Check the code.');
+            //  console.log('Solenoid has not been loaded. Check the code.');
 }
 // this is the INI files for solenoid. This will be implemented later.
