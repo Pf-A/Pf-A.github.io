9e073ad6421727fb9941fa4dc739e91c7c301703
Pf-A <M__carlson1345@outlook.com>
2025-06-06 15:20:24 -0500

    Update onlineChecker.js

--- SnakePlus/onlineChecker.js
+++ SnakePlus/onlineChecker.js
@@ -1,5 +1,6 @@
 function checkServerOnEXT() {
   superlog('GitHub checker: ONLINE')
+  var serverCheck = true;
   var statusElem = document.getElementById('online-status');
   if (serverCheck === true) {
                     statusElem.textContent = "Status: Online";
